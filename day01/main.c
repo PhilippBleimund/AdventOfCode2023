b7130ea1c3a98ca2e2335359e09fef6caea20612
Philipp Bleimund <bleimund.philipp@gmail.com>
2023-12-03 15:23:38 +0100

    added second answer

--- day01/main.c
+++ day01/main.c
@@ -10,6 +10,8 @@ char **lines;
 size_t total_lines = 0;
 size_t total_chars = 0;
 
+char numbers[9][5] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
 int calculate01(){
     int sum = 0;
 
@@ -18,14 +20,14 @@ int calculate01(){
         printf("line lenght: %zu | ", line_lenght);
         char left_most;
         char right_most;
-        for(size_t j = 0;j < line_lenght; j++){
+        for(int j = 0;j < line_lenght; j++){
             if(lines[i][j] <= '9' && lines[i][j] != '\n'){
                 left_most = lines[i][j];
                 printf("%c", left_most);
                 break;
             }
         }
-        for(size_t j = line_lenght-1;j >= 0; j--){
+        for(int j = line_lenght-1;j >= 0; j--){
             if(lines[i][j] <= '9' && lines[i][j] != '\n'){
                 right_most = lines[i][j];
                 printf(" %c", right_most);
@@ -36,6 +38,99 @@ int calculate01(){
         int sub_sum = 0;
         sub_sum = (left_most - '0') * 10;
         sub_sum += right_most - '0';
+        printf("   sum: %d \n", sub_sum);
+
+        sum += sub_sum;
+    }
+    return sum;
+}
+
+int calculate02(){
+    int sum = 0;
+
+    // scope one line
+    for(size_t i = 0; i < total_lines; i++){
+        size_t line_lenght = strlen(lines[i]);
+        printf("line lenght: %zu | ", line_lenght);
+
+        char left_most = '0';
+        char right_most = '0';
+        size_t left_most_index = line_lenght;
+        size_t right_most_index = 0;
+        int left_most_str = 0;
+        int right_most_str = 0;
+
+        for(int j = 0;j < line_lenght; j++){
+            if(lines[i][j] <= '9' && lines[i][j] != '\n'){
+                left_most = lines[i][j];
+                left_most_index = j;
+                printf("%c", left_most);
+                break;
+            }
+        }
+        for(int j = line_lenght-1;j >= 0; j--){
+            if(lines[i][j] <= '9' && lines[i][j] != '\n'){
+                right_most = lines[i][j];
+                right_most_index = j;
+                printf(" / %c", right_most);
+                break;
+            }
+        }
+
+        int *final_pos_min = malloc(sizeof(int) * 9);
+        int *final_pos_max = malloc(sizeof(int) * 9);
+        // scope one number
+        for(size_t j = 0; j < 9; j++){
+            int *pos = malloc(line_lenght);
+            int counter = 0;
+            // scope for every occurance of number
+            for(size_t k = 0; k < line_lenght; k++){
+                char *position_ptr = strstr(lines[i]+k, numbers[j]);
+
+                if(position_ptr != NULL){
+                    int position = position_ptr - lines[i];
+                    pos[counter] = position;
+                    counter++;
+                }
+            }
+            int current_min = line_lenght;
+            int current_max = 0;
+            for(int k = 0; k < counter; k++){
+                if(pos[k] < current_min)
+                    current_min = pos[k];
+                if(pos[k] > current_max)
+                    current_max = pos[k];
+            }
+            final_pos_min[j] = current_min;
+            final_pos_max[j] = current_max;
+        }
+
+        // determine the winner min number and winner max number
+        int left_most_str_index = line_lenght;
+        int right_most_str_index = 0;
+        for(int j = 0; j < 9; j++){
+            if(final_pos_min[j] < left_most_str_index){
+                left_most_str_index = final_pos_min[j];
+                left_most_str = j+1;
+            }
+            if(final_pos_max[j] > right_most_str_index){
+                right_most_str_index = final_pos_max[j];
+                right_most_str = j+1;
+            }
+        }
+        
+        // calculate final number
+        int sub_sum = 0;
+        if(left_most_str_index < left_most_index){
+            sub_sum = left_most_str * 10;
+        }else{
+            sub_sum = (left_most - '0') * 10;
+        }
+        if(right_most_str_index > right_most_index){
+            sub_sum += right_most_str;
+        }else{
+            sub_sum += right_most - '0';
+        }
         printf("   sum: %d \n", sub_sum);
 
         sum += sub_sum;
@@ -108,7 +203,7 @@ int main(void){
     //for (size_t i = 0; i < total_lines; i++)
     //    printf("%s", lines[i]);
 
-    int total_sum = calculate01();
+    int total_sum = calculate02();
     printf("\n\ntotal Sum: %d\n", total_sum);
 
     // Free the block of memory allocated for each string
